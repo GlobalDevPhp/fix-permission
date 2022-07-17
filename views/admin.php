c0779da741ad4ffaa4acdb53a530babe8a532cff
Yevhen <itnotes.org.ua@gmail.com>
2022-07-17 13:02:32 +0300

    Fix form usability

--- views/admin.php
+++ views/admin.php
@@ -16,6 +16,9 @@ if (!empty($FP->loop_obj)) {
     $paths = $FP->loop_obj->getPaths();
     $logs = implode(PHP_EOL, $FP->loop_obj->log);
 }
+$selected_flag = '0764';
+if (isset($_POST['permission_flag']))
+    $selected_flag = $_POST['permission_flag'];
 ?>
 <div class="wrap">
     <h2><?php esc_html_e( 'Fix file permissions', 'fix-permission' ); ?></h2>
@@ -32,24 +35,24 @@ wp-content/cache/index.html'; ?></textarea>
         <div>
             <p><?php esc_html_e( 'Select type the action:', 'fix-permission' ); ?></p>
             <select id="type_action" name="action_type">
-                <option value="permission" selected="selected"><?php esc_html_e( 'Edit permission', 'fix-permission' ); ?></option>
-                <option value="deletion"><?php esc_html_e( 'Deleting', 'fix-permission' ); ?></option>
+                <option value="permission" <?php echo (!empty($_POST['action_type']) && $_POST['action_type'] == 'permission')?'selected="selected"':'';?>><?php esc_html_e( 'Edit permission', 'fix-permission' ); ?></option>
+                <option value="deletion" <?php echo (!empty($_POST['action_type']) && $_POST['action_type'] == 'deletion')?'selected="selected"':'';?>><?php esc_html_e( 'Deleting', 'fix-permission' ); ?></option>
             </select>
             
             <select id="perm_mode" name="permission_flag">
                 <?php foreach (UpdatePermission::$permission_flags AS $name => $mod):?>                
-                    <option value="<?php echo $name; ?>" <?php echo $name == '0764'? 'selected="selected"' : ''; ?>><?php echo $name; ?></option>
+                    <option value="<?php echo $name; ?>" <?php echo $name == $selected_flag? 'selected="selected"' : ''; ?>><?php echo $name; ?></option>
                 <?php endforeach; ?>
             </select>
             <select id="type_action" name="test_mode">
-                <option value="true" selected="selected"><?php esc_html_e( 'Test mode On', 'fix-permission' ); ?></option>
-                <option value="false"><?php esc_html_e( 'Test mode Off', 'fix-permission' ); ?></option>
+                <option value="true" <?php echo (!empty($_POST['test_mode']) && $_POST['test_mode'] == 'true')?'selected="selected"':'';?>><?php esc_html_e( 'Test mode On', 'fix-permission' ); ?></option>
+                <option value="false" <?php echo (!empty($_POST['test_mode']) && $_POST['test_mode'] == 'false')?'selected="selected"':'';?>><?php esc_html_e( 'Test mode Off', 'fix-permission' ); ?></option>
             </select>
         </div>
         <div> 
             <p><?php esc_html_e( 'You want to perform an action on subfolders and files?', 'fix-permission' ); ?></p>
             <label for="recursion_on"><?php esc_html_e( 'Apply action recursively?', 'fix-permission' ); ?></label>                        
-            <input type="checkbox" id="recursion_on" name="recursion_on" value="yes">
+            <input type="checkbox" id="recursion_on" name="recursion_on" value="yes" <?php echo !empty($_POST['recursion_on'])?'checked="checked"':'';?>>
 
         </div>
         <div>
